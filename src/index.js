136e5cc27c1d881de885cbe53c2eb2bcfdf9ffb0
Mikael Hallgren <hallgren.mikael@gmail.com>
2019-11-18 14:47:30 +0100

    Update linter rules

--- src/index.js
+++ src/index.js
@@ -1,30 +1,45 @@
-const config = (override = {}) => ({
-  extends: ['airbnb'],
-  parserOptions: {
-    ecmaVersion: 2017,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  env: {
-    es6: true,
-    browser: true,
-    node: true,
-  },
-  parser: 'babel-eslint',
-  rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    quotes: [
-      2,
-      'single',
-      {
-        avoidEscape: true,
-        allowTemplateLiterals: true,
+const config = (override = {}) => {
+  // Deconstruct theses attrs to limit what you can extend linter with
+  const {
+    env,
+    rules,
+    parserOptions,
+    ...restOverride
+  } = override;
+
+  const result = ({
+    extends: ['airbnb'],
+    parserOptions: {
+      ecmaVersion: 2017,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
       },
-    ],
-  },
-  ...override,
-});
+    },
+    env: {
+      es6: true,
+      browser: true,
+      node: true,
+    },
+    parser: 'babel-eslint',
+    rules: {
+      'react/state-in-constructor': 0,
+      'react/static-property-placement': 0,
+      'react/jsx-props-no-spreading': 0,
+      'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+      quotes: [
+        2,
+        'single',
+        {
+          avoidEscape: true,
+          allowTemplateLiterals: true,
+        },
+      ],
+    },
+    ...restOverride,
+  });
+
+  return result;
+};
 
 module.exports = config;
